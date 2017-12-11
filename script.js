b3b0fccb82f3b7227cb4265a1f72c7dacf8a3764
kiran aranha <kiranaranha@students.berkeley.net>
2017-12-11 12:20:07 -0800

    Mostly Finished

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-
 function calcGrade(){
     var currentGrade = 0;
     var weight = 0;
     var grade = 0;
     var addedUpWeight = 0;
     var gradeTooHigh = false;
-
     for(var i = 1; i <= 4; i++) {
-        weight = parseInt(document.getElementById("weight" + i).value);
+        weight = parseFloat(document.getElementById("weight" + i).value);
         addedUpWeight += weight;
-        grade = parseInt(document.getElementById("grade" + i).value);
+        grade = parseFloat(document.getElementById("grade" + i).value);
         if(grade > 100 || isNaN(grade)){
             gradeTooHigh = true;
         }
@@ -18,22 +16,23 @@ function calcGrade(){
     if(addedUpWeight != 100){
         document.getElementById("currentGrade").innerHTML = "Please make sure % weight values are numbers and add up to 100. Enter correct data and try again!";
     }else if(gradeTooHigh){
-        document.getElementById("currentGrade").innerHTML = "Please make sure % grade values are numbers less than 100. Enter correct data and try again!";
+        document.getElementById("currentGrade").innerHTML = "Please make sure % grade values are numbers less than or equal to 100. Enter correct data and try again!";
     }else{
-        document.getElementById("currentGrade").innerHTML = "Your current grade is " + Math.round(currentGrade * 100) / 100.0 + "%";
+        document.getElementById("currentGrade").innerHTML = "Your current grade is " + Math.round(currentGrade * 100) / 100 + "%";
     }
     return currentGrade;
 }
 
-function calcFinal(){
+function calcGradeNeeded(){
     var currentGrade = calcGrade();
     var finalWeight = document.getElementById("finalWeight").value;
     var desiredGrade = document.getElementById("desiredGrade").value;
     var gradeNeeded = 0;
 
-    gradeNeeded = (desiredGrade - ((100 - finalWeight) * 0.01 * currentGrade)) / (finalWeight * 0.01);
-
-
-
-    document.getElementById("gradeNeeded").innerHTML = "You need at least a " + Math.round(gradeNeeded * 100) / 100 + "% on the final to get a " + desiredGrade + "% in the class.";
+    if(gradeNeeded > 100 || isNaN(gradeNeeded) || desiredGrade > 100 || isNaN(desiredGrade)){
+        document.getElementById("gradeNeeded").innerHTML = "Please make sure your data values are numbers less than or equal to 100. Enter Correct data values and try again!"
+    }else{
+        gradeNeeded = (desiredGrade - ((100 - finalWeight) * 0.01 * currentGrade)) / (finalWeight * 0.01);
+        document.getElementById("gradeNeeded").innerHTML = "You need at least a " + Math.round(gradeNeeded * 100) / 100 + "% on the final to get a " + desiredGrade + "% in the class.";
+    }
 }
